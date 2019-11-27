ab3043d04ffc123157f429c4eed90e179cf33c16
Merle66 <37998927+Merle66@users.noreply.github.com>
2019-11-27 11:37:39 +0100

    Add files via upload

---
+++ TadFecha/cmake-build-debug/CMakeFiles/TadFecha.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TadFecha.dir/main.cpp.obj"
+  "TadFecha.pdb"
+  "TadFecha.exe"
+  "TadFecha.exe.manifest"
+  "libTadFecha.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TadFecha.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
